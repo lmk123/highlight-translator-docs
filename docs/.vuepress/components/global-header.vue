7b41434e6943f534c61104d22fae592faea0e423
lmk123 <me@limingkai.cn>
2021-03-05 14:53:21 +0800

    docs: 修改无响应问题的公告

--- docs/.vuepress/components/global-header.vue
+++ docs/.vuepress/components/global-header.vue
@@ -1,5 +1,5 @@
 <template>
   <div id="global-header">
-    <a href="https://github.com/lmk123/crx-selection-translate/issues/916" target='_blank' style="color:red">关于近期很多用户反馈划词翻译会导致浏览器无响应或崩溃的情况，请点击这里查看解决方案。</a>
+    <a href="https://hcfy.limingkai.cn/install.html" style="color:red">关于近期很多用户反馈划词翻译会导致浏览器无响应或崩溃的情况，等浏览器自动更新到 v7.5.2 或手动下载 v7.5.2 的离线安装包即可解决。</a>
   </div>
 </template>
