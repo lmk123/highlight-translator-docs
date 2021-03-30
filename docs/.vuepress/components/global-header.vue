9515cfc53ba82553239aafcc453e212054af5b2f
lmk123 <me@limingkai.cn>
2021-03-30 15:06:35 +0800

    docs: 去掉 global-header
    现在划词翻译已经支持通知中心了，所以文档站里的 global-header 可以去掉了

--- docs/.vuepress/components/global-header.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div id="global-header">
-    <a href="https://hcfy.limingkai.cn/install.html" style="color:red">划词翻译让浏览器无响应或崩溃的问题已经在 v7.5.2 中解决了，你可以等浏览器自动更新或手动下载离线安装包安装。</a>
-  </div>
-</template>
