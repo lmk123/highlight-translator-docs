3f8d594879d41e29b543ecbf2830aa5fdecaa6be
lmk123 <me@limingkai.cn>
2021-06-09 07:17:34 +0800

    feat: 给导航栏的链接添加尾斜线

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,12 +24,12 @@ module.exports = {
         src: 'logo.png',
       },
       items: [
-        { to: '/docs/install', label: '安装', position: 'right' },
-        { to: '/docs/faq', label: '常见问题', position: 'right' },
-        { to: '/docs/services/intro', label: '服务申请', position: 'right' },
-        { to: '/docs/log', label: '更新日志', position: 'right' },
-        { to: '/docs/issues', label: '反馈问题', position: 'right' },
-        { to: '/blog', label: '产品博客', position: 'right' },
+        { to: '/docs/install/', label: '安装', position: 'right' },
+        { to: '/docs/faq/', label: '常见问题', position: 'right' },
+        { to: '/docs/services/intro/', label: '服务申请', position: 'right' },
+        { to: '/docs/log/', label: '更新日志', position: 'right' },
+        { to: '/docs/issues/', label: '反馈问题', position: 'right' },
+        { to: '/blog/', label: '产品博客', position: 'right' },
         {
           href: 'https://github.com/lmk123/crx-selection-translate',
           label: '查看源码',
